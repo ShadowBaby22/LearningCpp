8a18605b303b3b11baff8113e80c5fae79ee3a43
Siddharth Gupta <50405111+ShadowBaby22@users.noreply.github.com>
2023-07-24 17:01:38 +0530

    Add files via upload

---
+++ TernaryOperator.cpp
@@ -0,0 +1,36 @@
+#include <iostream> 
+using namespace std;
+
+enum hello{
+    a,b=99,c,d=-1
+};
+
+enum color{
+    blue,red
+};
+
+enum game{
+//goop=70, poog=101.0
+};
+
+int main () {
+        
+        //cout<<(5 < 8 ? 1 !=2 < 5==0 ? 10:20:30);
+
+        int test=0;
+        cout<<'1';
+        cout<< (test ? 3:'1') << '\n'; //149? ASCII Value? 
+        
+
+    enum hello m;
+    cout <<a << b<< c<< d << '\n';
+
+    cout<< sizeof(color);
+
+    //cout <<poog;
+
+    cout << 'a' + 'I';
+
+    return 0;
+
+}
